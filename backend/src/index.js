9687eb586c5131126b1054066ede22d7ac24762d
erick-andrade1 <erickandradesilva@hotmail.com>
2022-04-02 10:02:22 -0300

    Servidor do backend subindo

---
+++ backend/src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require("cors");
+const app = express();
+const PORT = 3001;
+
+app.use(cors({}));
+
+app.listen(PORT, console.log(`Server online in PORT = ${PORT}`))
